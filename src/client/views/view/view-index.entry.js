2ead4a280da8e325f8626989aace9b7850e3a0ff
ranjayzheng <ranjayzheng@qq.com>
2018-12-27 20:49:52 +0800

    add x-tag framework

---
+++ src/client/views/view/view-index.entry.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+xtag.register('x-header', {
+    content: `<nav id="w1" class="navbar-inverse navbar-fixed-top navbar">
+    <div class="container">
+        <div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target="#w1-collapse"><span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span></button><a class="navbar-brand" href="#">My
+                Application</a>
+        </div>
+        <div id="w1-collapse" class="collapse navbar-collapse">
+            <ul id="w2" class="navbar-nav navbar-right nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>`
+});
+xtag.register('x-footer', {
+    content: `<footer class="footer">
+    <div class="container">
+        <p class="pull-left">© My Company 2018</p>
+        <p class="pull-right">Powered by <a href="http://www.yiiframework.com/" rel="external">Yii Framework</a></p>
+    </div>
+</footer>`
+});
