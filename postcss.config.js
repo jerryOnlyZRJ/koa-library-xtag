b1f01b0577021125abe03d1e29b6b158d105eb46
ranjayzheng <ranjayzheng@qq.com>
2019-01-23 14:07:03 +0800

    remove autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 module.exports = {
 	plugins: {
 		'postcss-preset-env': {
-			stage: 0
-		},
-		"autoprefixer": {
-			browsers: ['> 1%', 'last 2 versions', 'Firefox >= 20', 'iOS >=7']
+			stage: 0,
+			browsers: ['> 1%']
 		},
 		'cssnano': {}
 	}
